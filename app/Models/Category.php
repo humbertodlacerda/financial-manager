26c1b3dbaec9dadcd386c4e78739425b63b2ce5f
humbertodlacerda <humbertodlacerda@gmail.com>
2022-11-30 13:06:43 -0300

    implements validations

---
+++ app/Models/Category.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Expense;
+use App\Models\Revenue;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $table = 'categories';
+    protected $filable = [
+        'description'
+    ];
+
+    /**
+     * Get the expense that owns the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function expense(): BelongsTo
+    {
+        return $this->belongsTo(Expense::class, 'category_id', 'id');
+    }
+
+    /**
+     * Get the revenue that owns the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function revenue(): BelongsTo
+    {
+        return $this->belongsTo(Revenue::class, 'category_id', 'id');
+    }
+}
